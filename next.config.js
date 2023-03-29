19a104a633926e289b83bdb24c5a69f73eeb5b87
Yair <yair.notkovich@gmail.com>
2023-03-30 00:54:12 +0300

    build 2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   reactStrictMode: true,
-  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
