86dbec568b9687beaca4b0ee47361d406286a157
Yair <yair.notkovich@gmail.com>
2023-04-06 15:37:17 +0300

    updated Head/next

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
         <Head>
           <title>Yair Full-Stack </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel='icon' href='/public/favicon.ico' />
+          <link rel='icon' href='/favicon.ico' />
         </Head>
         <Navbar setShowResume={setShowResume} theme={dark} setDark={setDark} />
         <Hero />
